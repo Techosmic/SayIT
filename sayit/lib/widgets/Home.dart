1c4675cee8bb5afd316b17bb9560d01d61ff7d93
Xavier Vercruysse <xavier.vercruysse@delaware.pro>
2019-11-23 21:53:56 +0100

    Testing background image

--- sayit/lib/widgets/Home.dart
+++ sayit/lib/widgets/Home.dart
@@ -10,48 +10,53 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text('SayIT'),
       ),
-      body: Center(
-        child: GridView.count(
-          crossAxisCount: 1,
-          mainAxisSpacing: 15,
-          childAspectRatio: 2.5,
-          crossAxisSpacing: 25,
-          padding: EdgeInsets.all(75),
-          children: < Widget > [
-            RaisedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LevelSelect()),
-                );
-              },
-              shape: RoundedRectangleBorder(
-                borderRadius: new BorderRadius.circular(18.0),
-                side: BorderSide(color: Colors.teal, width: 2),
-              ),
-              child: Text(
-                'Aventure',
-                style: Body1Style
-              ),
+      body: Stack(
+        children: < Widget > [
+          Image.asset('lib/assets/logo.png'),
+          Center(
+            child: GridView.count(
+              crossAxisCount: 1,
+              mainAxisSpacing: 15,
+              childAspectRatio: 2.5,
+              crossAxisSpacing: 25,
+              padding: EdgeInsets.all(75),
+              children: < Widget > [
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LevelSelect()),
+                    );
+                  },
+                  shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(18.0),
+                    side: BorderSide(color: Colors.teal, width: 2),
+                  ),
+                  child: Text(
+                    'Aventure',
+                    style: Body1Style
+                  ),
+                ),
+                RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(18.0),
+                    side: BorderSide(color: Colors.teal, width: 2),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => PlayerWidget()),
+                    );
+                  },
+                  child: Text(
+                    'Entrainement',
+                    style: Body1Style
+                  ),
+                ),
+              ],
             ),
-            RaisedButton(
-              shape: RoundedRectangleBorder(
-                borderRadius: new BorderRadius.circular(18.0),
-                side: BorderSide(color: Colors.teal, width: 2),
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => PlayerWidget()),
-                );
-              },
-              child: Text(
-                'Entrainement',
-                style: Body1Style
-              ),
-            ),
-          ],
-        ),
+          )
+        ],
       )
     );
   }
